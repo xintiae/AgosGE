057e35900f2727d2d93d78c909b234fb34d6bed1
xintiae <cyprien.debeauvais@hotmail.com>
2021-05-05 20:14:23 +0200

    Added CMakeLists

--- Agos/agos.h
+++ Agos/agos.h
@@ -1,3 +1,6 @@
+#pragma once
 // --- include file for AgosGE ---
 
+#include "src/base.h"
+#include "src/logger.h"
 #include "src/entry_point.h"
---
+++ examples/init_agosge.cpp
@@ -0,0 +1,9 @@
+#include "Agos/agos.h"
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+    std::cout << "Hello, world from file " << __FILE__ << "!\n";
+    std::cin.get();
+    return 0;
+}
