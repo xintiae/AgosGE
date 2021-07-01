1aa2caabe931535a82f171ddb069c81f394215b2
Dodopriester <lp.toadiboy@gmail.com>
2021-07-01 18:55:13 +0200

    Updated scripts/build_windows.bat

--- scripts/build_windows.bat
+++ scripts/build_windows.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-echo "Updating submodules..."
+echo Updating submodules...
 git submodule init
 git submodule update
 
@@ -8,12 +8,12 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`"%ProgramFiles(x86)%\Microsoft Visual Studio
 SET MSBuildPath=%%F
 )
 
-echo "Creating Visual Studio Files..."
+echo Creating Visual Studio Files...
 cd ..
 if not exist build mkdir build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Debug ..
-echo "Building AgosGE..."
+echo Building AgosGE...
 "%MSBuildPath%" AgosGE.sln
 
 pause
