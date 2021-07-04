9f09379f980dfd03e1244da59b60876f603dc487
Dodopriester <lp.toadiboy@gmail.com>
2021-07-04 17:04:32 +0200

    Updated windows script

--- scripts/build_windows.bat
+++ scripts/build_windows.bat
@@ -4,6 +4,9 @@ echo Updating submodules...
 git submodule init
 git submodule update
 
+echo Cleaning build directory...
+if exist build del build/*.*
+
 FOR /F "tokens=* USEBACKQ" %%F IN (`"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -latest -prerelease -products * -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`) DO (
 SET MSBuildPath=%%F
 )
